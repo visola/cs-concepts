bad1e259e9a62afd20003f4d2fd5b9450c76d029
Vinicius Isola <viniciusisola@gmail.com>
2017-06-16 08:17:00 -0400

    Refactored Fibonacci to fit multiple implementations

---
+++ src/main/java/org/visola/cs/algorithms/math/IterativeFibonacciSeries.java
@@ -0,0 +1,48 @@
+package org.visola.cs.algorithms.math;
+
+/**
+ * Implementation of the Fibonacci Series using iterative algorithms and no
+ * memoization.
+ */
+public class IterativeFibonacciSeries implements FibonacciSeries {
+
+  public static void main (String [] args) {
+    IterativeFibonacciSeries fibo = new IterativeFibonacciSeries();
+    for (int i = 0; i < 200; i++) {
+      System.out.print(fibo.sumUpTo(i) + "L, ");
+    }
+  }
+
+  public long getNumberAt(long position) {
+    if (position == 0 || position == 1) {
+      return 1;
+    }
+    long previous = 1;
+    long current = 1;
+    int i = 1;
+    while (i < position) {
+      long temp = previous + current;
+      previous = current;
+      current = temp;
+      i++;
+    }
+    return current;
+  }
+
+  public long sumUpTo(long position) {
+    long result = 1;
+
+    int i = 0;
+    long previous = 1;
+    long current = 1;
+    while (i < position) {
+      result += current;
+      long temp = previous + current;
+      previous = current;
+      current = temp;
+      i++;
+    }
+    return result;
+  }
+
+}
---
+++ src/test/java/org/visola/cs/algorithms/math/FibonacciSeriesTest.java
@@ -0,0 +1,51 @@
+package org.visola.cs.algorithms.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FibonacciSeriesTest {
+
+  private static final Class<? extends FibonacciSeries> [] ALL_IMPLEMENTATIONS = new Class [] {
+    IterativeFibonacciSeries.class
+  };
+
+  private List<FibonacciSeries> allSeries = new ArrayList<>();
+
+  @Before
+  public void setup() throws Exception {
+    for (Class<? extends FibonacciSeries> c : ALL_IMPLEMENTATIONS) {
+      allSeries.add(c.newInstance());
+    }
+  }
+
+  @Test
+  public void calculateNthFibonacciNumber() {
+    for (FibonacciSeries fibonacciSeries : allSeries) {
+      for (int index = 0; index < FibonacciSeries.FIBONACCI_NUMBERS.length; index++) {
+        assertEquals(
+          "Wrong Fibonacci number at "+index+" for series "+fibonacciSeries.getClass().getName(),
+          FibonacciSeries.FIBONACCI_NUMBERS[index],
+          fibonacciSeries.getNumberAt(index)
+        );
+      }
+    }
+  }
+
+  @Test
+  public void calculateSumOfNthFibonacciNumbers() {
+    for (FibonacciSeries fibonacciSeries : allSeries) {
+      for (int index = 0; index < FibonacciSeries.FIBONACCI_SUM.length; index++) {
+        assertEquals(
+          "Wrong sum result at "+index+" for series "+fibonacciSeries.getClass().getName(),
+          FibonacciSeries.FIBONACCI_SUM[index],
+          fibonacciSeries.sumUpTo(index)
+        );
+      }
+    }
+  }
+
+}
